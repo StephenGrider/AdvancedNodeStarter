c259f941d16d1f19c073ce2c96684838bcd3f792
stephengrider <ste.grider@gmail.com>
2018-01-23 11:32:00 -0800

    jest

--- config/prod.js
+++ config/prod.js
@@ -4,8 +4,5 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN
 };
