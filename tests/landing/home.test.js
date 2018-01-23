fbc6e0bae06059218b3244698cb6cec87cbc9777
stephengrider <ste.grider@gmail.com>
2018-01-23 12:13:16 -0800

    debuggin

--- tests/landing/home.test.js
+++ tests/landing/home.test.js
@@ -18,7 +18,7 @@ afterEach(async () => {
 test('Loads the homepage', async () => {
   const p = await page.goto('localhost:3000').html();
 
-  console.log(html);
+  console.log(p);
   const h1 = await page.evaluate(() => document.querySelector('h1').innerHTML);
 
   expect(h1).toEqual('Emaily!');
