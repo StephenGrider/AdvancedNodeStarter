5df35b9999c9f792056f9ab6fdb307eb7c579809
stephengrider <ste.grider@gmail.com>
2018-01-24 13:31:58 -0800

    working chromeless

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -3,7 +3,8 @@ const url = require('../url');
 
 module.exports = () => {
   const page = new Chromeless({
-    remote: false, //process.env.NODE_ENV === 'production',
+    // remote: false, //process.env.NODE_ENV === 'production',
+    remote: process.env.TEST_MODE === 'fast',
     implicitWait: true
   });
 
