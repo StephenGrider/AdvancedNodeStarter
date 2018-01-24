9d96fbc51bb12785f62ad1054d1bcce843beef6c
stephengrider <ste.grider@gmail.com>
2018-01-24 09:39:48 -0800

    not remote

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -2,7 +2,7 @@ const { Chromeless } = require('chromeless');
 
 module.exports = () => {
   return new Chromeless({
-    remote: process.env.NODE_ENV === 'production',
+    remote: false, //process.env.NODE_ENV === 'production',
     implicitWait: true
   });
 };
