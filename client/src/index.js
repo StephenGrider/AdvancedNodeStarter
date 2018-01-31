d4ac2608b4238ed564a739e524308d12e6e63c05
stephengrider <ste.grider@gmail.com>
2018-01-31 09:59:49 -0800

    aws codepipeline setup

--- client/src/index.js
+++ client/src/index.js
@@ -8,13 +8,11 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-// Development only axios helpers!
-import axios from 'axios';
-window.axios = axios;
-
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
