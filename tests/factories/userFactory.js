dc2d1993fbddcabf82733fa882e719db5c933fad
stephengrider <ste.grider@gmail.com>
2018-01-31 11:11:13 -0800

    force rebuild

--- tests/factories/userFactory.js
+++ tests/factories/userFactory.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 module.exports = (opts = {}) => {
   const User = mongoose.model('users');
 
-  return new User(Object.assign({}, {
-    googleId: '1'
-  }, opts).save();
+  return new User(
+    Object.assign(
+      {},
+      {
+        googleId: '1'
+      },
+      opts
+    )
+  ).save();
 };
