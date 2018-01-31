af1ce74664e9103e9bfc80cfad4381973a9f2289
stephengrider <ste.grider@gmail.com>
2018-01-31 11:34:13 -0800

    key tweaks

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,12 @@
 // prod.js - production keys here!!
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  redirectDomain: process.env.REDIRECT_DOMAIN
+  // googleClientID: process.env.GOOGLE_CLIENT_ID,
+  // googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  // mongoURI: process.env.MONGO_URI,
+  // cookieKey: process.env.COOKIE_KEY,
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://stephen:password@ds111598.mlab.com:11598/blog_testing',
+  cookieKey: 'ihfkjasdhfkaushfkaurngakeurgkaeurhgkaeuhrgkaerhg'
 };
