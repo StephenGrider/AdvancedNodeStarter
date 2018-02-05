e1dc5faf199267da85423c31f0e80da52382f6f9
stephengrider <ste.grider@gmail.com>
2018-02-05 10:24:32 -0800

    remove dirname

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
 
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
 
