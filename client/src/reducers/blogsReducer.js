6985764db3b7da24b54bfa83091407362574dbc6
stephengrider <ste.grider@gmail.com>
2018-02-05 10:15:12 -0800

    pre-refactor

--- client/src/reducers/blogsReducer.js
+++ client/src/reducers/blogsReducer.js
@@ -5,7 +5,6 @@ export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_BLOG:
       const blog = action.payload;
-
       return { ...state, [blog._id]: blog };
     case FETCH_BLOGS:
       return { ...state, ...mapKeys(action.payload, '_id') };
