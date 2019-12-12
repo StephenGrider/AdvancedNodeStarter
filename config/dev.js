f740a99ccc430288384f405fbe510b0791d2d34d
Stephen Grider <ste.grider@gmail.com>
2019-12-12 11:33:36 -0800

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://stephen:password@ds111598.mlab.com:11598/blog_testing',
-  cookieKey: 'ihfkjasdhfkaushfkaurngakeurgkaeurhgkaeuhrgkaerhg'
+  mongoURI: '<YOUR MONGO URI>',
+  cookieKey: '<YOUR COOKIE KEY>'
 };
