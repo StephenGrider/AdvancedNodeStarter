5aed2c1c90471b2bb3c57e26cac551e08c935eb0
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-03-31 12:22:04 -0400

    Initial commit

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientID: 
+  '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: "mongodb+srv://vladdic:eternity72@cluster0-yehuc.mongodb.net/test?retryWrites=true&w=majority",
   cookieKey: '123123123'
 };
