c866e1b504aab052b303d2d15c30b9576ac6ab8d
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-03-31 20:12:26 -0400

    redis logic added to blogRoutes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,28 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    const redis = require('redis')
+    const redisUrl = 'redis://127.0.0.1:6379'
+    const client = redis.createClient(redisUrl)
+    const util = require('util')
+    client.get = util.promisify(client.get)
 
+    //Do we have any cashed data in redis related to this query
+    const cashedBlogs = await client.get(req.user.id)
+    
+    //if yes, then respond to the request right away and return
+    if (cashedBlogs) {
+      console.log('Serving from cache')
+      return res.send(JSON.parse(cashedBlogs))
+    }
+
+    //if no, we need to respond to request and update our cache to store the data
+    
+    const blogs = await Blog.find({ _user: req.user.id });
+     console.log('Serving from Mongodb')
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs))
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
