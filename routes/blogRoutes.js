1ea9a740fe463038d745d5a7d55d349a8ffee8d3
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-04-02 16:25:38 -0400

    added key to app.get(/api/blogs)

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -15,7 +15,9 @@ module.exports = app => {
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
 
-    const blogs = await Blog.find({ _user: req.user.id }).cache();
+    const blogs = await Blog
+      .find({ _user: req.user.id })
+      .cache({key: req.user.id });
 
     res.send(blogs)
 
