6045e4a72c39e3f65f5a80a7e9dea2322a956dc9
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-04-02 13:48:37 -0400

    added toggleable cache

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -15,7 +15,7 @@ module.exports = app => {
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
 
-    const blogs = await Blog.find({ _user: req.user.id });
+    const blogs = await Blog.find({ _user: req.user.id }).cache();
 
     res.send(blogs)
 
--- services/cache.js
+++ services/cache.js
@@ -7,12 +7,21 @@ const client = redis.createClient(redisUrl)
 client.get = util.promisify(client.get)
 const exec = mongoose.Query.prototype.exec
 
+mongoose.Query.prototype.cache = function() {
+    this.useCache = true;
+    return this;
+}
+
 //using keyword function here because arrow function tries to mess around with the value of this
 //inside the function. This is a function assigned to prototype.While *this* should reference the Query being produced
 mongoose.Query.prototype.exec = async function () {
-    console.log('I am about to run a query');
+    if (!this.useCache) {
+        return exec.apply(this, arguments)
+    }
+    // console.log('I am about to run a query');
     // console.log(this.getQuery());
     // console.log(this.mongooseCollection.name);
+
     const key = JSON.stringify(
         Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
