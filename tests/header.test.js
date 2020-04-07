bf0d989b2e57a303c4beaf5edee31fb0a8138c9e
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-04-07 15:17:27 -0400

    oath flow test added

--- tests/header.test.js
+++ tests/header.test.js
@@ -14,8 +14,15 @@ afterEach(async () => {
     await browser.close()
 })
 
-test('We can launch a browser', async () => {
+test('the header has the correct text', async () => {
 
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster')
+});
+
+test('clicking login starts oath flow', async () => {
+    await page.click('.right a');
+    const url = await page.url()
+
+    expect(url).toMatch(/accounts\.google\.com/);
 })
