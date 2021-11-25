f269c54d9b90fe3740119f8cca33cb38e8e45465
prameet mandal <83633832+prameetSP@users.noreply.github.com>
2021-11-25 19:24:32 +0530

    changes

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,8 @@
 module.exports = {
   googleClientID:
-    '463735865171-g7sehopdfnvm9e84sen4i5dc4q82pf30.apps.googleusercontent.com',
-  googleClientSecret: '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',
+    'add-your-key',
+  googleClientSecret: 'add-secret-key',
   //mongoURI: 'mongodb+srv://prameet:O6MsOG0J6xWk0Oa9@cluster0.ejmkd.mongodb.net/blog_dev?retryWrites=true&w=majority',
-  mongoURI: 'mongodb://prameet:O6MsOG0J6xWk0Oa9@cluster0-shard-00-00.ejmkd.mongodb.net:27017,cluster0-shard-00-01.ejmkd.mongodb.net:27017,cluster0-shard-00-02.ejmkd.mongodb.net:27017/blog_dev?ssl=true&replicaSet=atlas-2x7zzn-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongoURI: ',mongo-url',
   cookieKey: '123123123',
 };
