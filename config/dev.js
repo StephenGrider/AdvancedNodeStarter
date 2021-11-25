4cdc55e5dc9e3aba1661157bcf28d6dde2478351
prameet mandal <83633832+prameetSP@users.noreply.github.com>
2021-11-25 19:24:54 +0530

    fix

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,6 @@ module.exports = {
   googleClientID:
     'add-your-key',
   googleClientSecret: 'add-secret-key',
-  //mongoURI: 'mongodb+srv://prameet:O6MsOG0J6xWk0Oa9@cluster0.ejmkd.mongodb.net/blog_dev?retryWrites=true&w=majority',
   mongoURI: ',mongo-url',
   cookieKey: '123123123',
 };
